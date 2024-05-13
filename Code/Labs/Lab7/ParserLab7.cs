f6b99157ade37447c951587fad4b478a51eca83c
Renyestai <akseleu@yahoo.com>
2024-05-13 12:15:45 +0700

    parser lab7 fixes

--- Code/Labs/Lab7/ParserLab7.cs
+++ Code/Labs/Lab7/ParserLab7.cs
@@ -1,5 +1,17 @@
 ﻿public class ParserLab7
 {
+    private List<Token> Tokens;
+    private int CurrentTokenIndex;
+	private StringBuilder result = new StringBuilder();
+	private const string sep1 = " → ";
+	public ParserLab7()
+    {
+        Tokens = new List<Token>();
+        CurrentTokenIndex = 0;
+    }
+
+    public void Parse(List<Token> tokens)
+    {
 
 }
 
